852c91b04cd28fd8157ada8ec26558e406f3ff6d
HR <hrui1990@gmail.com>
2018-10-19 00:39:34 -0500

    set up resolvers

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,14 @@
-import "reflect-metadata"; //Keeping this line for typeORM
+import "reflect-metadata"; // Keeping this line for typeORM
+import { importSchema } from "graphql-import";
 import { GraphQLServer } from "graphql-yoga";
+import { resolvers } from "./resolvers";
+import * as path from "path";
+// use this to resolve the problem with paths when using importSchema
+import { createConnection } from "typeorm";
 
-const typeDefs = `
-  type Query {
-    hello(name: String): String!
-  }
-`;
-
-const resolvers = {
-  Query: {
-    hello: (_: any, { name }: any) => `Hello ${name || "World"}`
-  }
-};
+const typeDefs = importSchema(path.join(__dirname, "./schema.graphql"));
 
 const server = new GraphQLServer({ typeDefs, resolvers });
-server.start(() => console.log("Server is running on localhost:4000"));
+createConnection().then(() => {
+  server.start(() => console.log("Server is running on localhost:4000"));
+});
